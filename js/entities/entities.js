aed06c3b6a36ab7f2fb7f43fd75a0854907a4f88
ar1367 <ar1367@georgetown.edu>
2016-06-17 10:52:59 -0400

    Update entities.js

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -9,8 +9,8 @@ var BirdEntity = me.Entity.extend({
 
         this._super(me.Entity, 'init', [x, y, settings]);
         this.alwaysUpdate = true;
-        this.body.gravity = 0.2;
-        this.gravityForce = 0.01;
+        this.body.gravity = 0.6;
+        this.gravityForce = 0.00;
         this.maxAngleRotation = Number.prototype.degToRad(30);
         this.maxAngleRotationDown = Number.prototype.degToRad(90);
         this.renderable.addAnimation("flying", [0, 1, 2]);
